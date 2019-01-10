7343587cf7313cca5e7e75a4b6acc0fc41479f24
amedeopro <amedeopro@me.com>
2019-01-10 14:57:39 +0100

    riorganizzate le chiamate Ajax in funzioni

--- script.js
+++ script.js
@@ -5,147 +5,15 @@ $(document).ready(function(){
       method: 'GET',
       success: function(data){
 
+          graficoLine(data);
+          graficoPie(data);
 
-        // var arrayMesi = [];
-        // var arrayImporti = [];
-        var oggettoVendite = {
-            January: 0,
-            February: 0,
-            March: 0,
-            April: 0,
-            May: 0,
-            June: 0,
-            July: 0,
-            August: 0,
-            September: 0,
-            October: 0,
-            November: 0,
-            December: 0
-        };
-
-        for (var i = 0; i < data.length; i++) {
-          var dataApi = data[i].date;
-          var importiApi = data[i].amount;
-          var prendiMese = moment(dataApi, 'DD/MM/YYYY');
-          var meseVendita = prendiMese.format('MMMM');
-
-          oggettoVendite[meseVendita] += importiApi
-
-        }
-
-          var arrayMesi = [];
-          var arrayImporti = [];
-
-          for (var mese in oggettoVendite) {
-            arrayMesi.push(mese);
-            arrayImporti.push(oggettoVendite[mese])
-          }
-
-
-        console.log(arrayMesi);
-        console.log(arrayImporti);
-
-        var ctx = $('#myChart');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'line',
-
-            // The data for our dataset
-            data: {
-                labels: arrayMesi,//["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [{
-                    label: "Vendite totali per mese",
-                    backgroundColor: '',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: arrayImporti,
-                }]
-            },
-
-            // Configuration options go here
-            options: {}
-        });
       },
       error: function(){
         alert('errore');
       }
     })
 
-
-
-
-    $.ajax({
-        url: apiUrl,
-        method: 'GET',
-        success: function(data){
-
-          var oggettoProcessato = {}
-
-          for (var i = 0; i < data.length; i++) {
-            var vendorsApi = data[i].salesman;
-            var importiApi = data[i].amount;
-
-
-            if (oggettoProcessato[vendorsApi] == undefined) {
-
-              oggettoProcessato[vendorsApi] = 0;
-
-            }
-
-            oggettoProcessato[vendorsApi] += importiApi;
-
-          }
-
-          var arrayVenditori = [];
-          var arrayImporti = [];
-
-          for (var nomeVenditore in oggettoProcessato) {
-
-            arrayVenditori.push(nomeVenditore);
-            arrayImporti.push(oggettoProcessato[nomeVenditore]);
-
-          }
-
-          var arrayPercentuali = [];
-          var totSales = totaleVendite(arrayImporti)
-          for (var i = 0; i < arrayImporti.length; i++) {
-            var num = (arrayImporti[i] / totSales)*100;
-            var numDueDecimali = num.toFixed(2);
-            arrayPercentuali.push(numDueDecimali);
-          }
-          console.log(arrayPercentuali)
-          console.log(arrayVenditori);
-          console.log(arrayImporti);
-          console.log(totaleVendite(arrayImporti));
-
-          var ctx = $('#myChart2');
-          var chart = new Chart(ctx, {
-              // The type of chart we want to create
-              type: 'doughnut',
-
-              // The data for our dataset
-              data: {
-                  labels: arrayVenditori,//["January", "February", "March", "April", "May", "June", "July"],
-                  datasets: [{
-                      label: arrayVenditori,
-                      backgroundColor: ['blue','yellow','red','green'],
-                      borderColor: 'rgb(255, 99, 132)',
-                      data: arrayPercentuali,
-                  }]
-              },
-
-              // Configuration options go here
-              options: {
-                cutoutPercentage: 20,
-              }
-          });
-
-
-
-        },
-        error: function(){
-          alert('errore');
-        }
-      })
 
       function totaleVendite(array){
         var totale = 0;
@@ -155,6 +23,130 @@ $(document).ready(function(){
         }
 
         return totale;
+      }
+
+      function graficoLine(risultato){
+
+                var oggettoVendite = {
+                    January: 0,
+                    February: 0,
+                    March: 0,
+                    April: 0,
+                    May: 0,
+                    June: 0,
+                    July: 0,
+                    August: 0,
+                    September: 0,
+                    October: 0,
+                    November: 0,
+                    December: 0
+                };
+
+                for (var i = 0; i < risultato.length; i++) {
+                  var dataApi = risultato[i].date;
+                  var importiApi = risultato[i].amount;
+                  var prendiMese = moment(dataApi, 'DD/MM/YYYY');
+                  var meseVendita = prendiMese.format('MMMM');
+
+                  oggettoVendite[meseVendita] += importiApi
+
+                }
+
+                  var arrayMesi = [];
+                  var arrayImporti = [];
+
+                  for (var mese in oggettoVendite) {
+                    arrayMesi.push(mese);
+                    arrayImporti.push(oggettoVendite[mese])
+                  }
+
+
+                console.log(arrayMesi);
+                console.log(arrayImporti);
+
+                var ctx = $('#myChart');
+                var chart = new Chart(ctx, {
+                    // The type of chart we want to create
+                    type: 'line',
+
+                    // The data for our dataset
+                    data: {
+                        labels: arrayMesi,//["January", "February", "March", "April", "May", "June", "July"],
+                        datasets: [{
+                            label: "Vendite totali per mese",
+                            backgroundColor: '',
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: arrayImporti,
+                        }]
+                    },
+
+                    // Configuration options go here
+                    options: {}
+                });
+      }
+
+      function graficoPie(risultato){
+
+                  var oggettoProcessato = {}
+
+                  for (var i = 0; i < risultato.length; i++) {
+                    var vendorsApi = risultato[i].salesman;
+                    var importiApi = risultato[i].amount;
+
+
+                    if (oggettoProcessato[vendorsApi] == undefined) {
+
+                      oggettoProcessato[vendorsApi] = 0;
+
+                    }
+
+                    oggettoProcessato[vendorsApi] += importiApi;
+
+                  }
+
+                  var arrayVenditori = [];
+                  var arrayImporti = [];
+
+                  for (var nomeVenditore in oggettoProcessato) {
+
+                    arrayVenditori.push(nomeVenditore);
+                    arrayImporti.push(oggettoProcessato[nomeVenditore]);
+
+                  }
+
+                  var arrayPercentuali = [];
+                  var totSales = totaleVendite(arrayImporti)
+                  for (var i = 0; i < arrayImporti.length; i++) {
+                    var num = (arrayImporti[i] / totSales)*100;
+                    var numDueDecimali = num.toFixed(2);
+                    arrayPercentuali.push(numDueDecimali);
+                  }
+                  console.log(arrayPercentuali)
+                  console.log(arrayVenditori);
+                  console.log(arrayImporti);
+                  console.log(totaleVendite(arrayImporti));
+
+                  var ctx = $('#myChart2');
+                  var chart = new Chart(ctx, {
+                      // The type of chart we want to create
+                      type: 'doughnut',
+
+                      // The data for our dataset
+                      data: {
+                          labels: arrayVenditori,//["January", "February", "March", "April", "May", "June", "July"],
+                          datasets: [{
+                              label: arrayVenditori,
+                              backgroundColor: ['blue','yellow','red','green'],
+                              borderColor: 'rgb(255, 99, 132)',
+                              data: arrayPercentuali,
+                          }]
+                      },
+
+                      // Configuration options go here
+                      options: {
+                        cutoutPercentage: 20,
+                      }
+                  });
       }
 
 
